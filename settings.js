818761c9665f7379fcbc6861816139c99f99124a
Nguroje01 <uqbahyunus@gmail.com>
2025-04-07 07:24:32 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'uqbah',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "uqbah Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '2348085957731', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
